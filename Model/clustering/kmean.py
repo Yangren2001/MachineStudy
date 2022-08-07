adeb8f9921a048ff829be8128f6454e4c24c99de
Yangren2001 <2431206446@qq.com>
2022-08-07 16:20:44 +0800

    first test
    Signed-off-by: Yangren2001 <2431206446@qq.com>

--- Model/clustering/kmean.py
+++ Model/clustering/kmean.py
@@ -13,10 +13,16 @@ from utils.utils import *
 
 class KMean(Model):
 
-    def __init__(self):
+    def __init__(self, cluster_num, init_cluster_center=None):
         super(KMean, self).__init__()
-        self.__cluster_center = None   # Model center
+        self.__cluster_center = []   # Model center
         self.__SSE = 0.0         # sum of squared error
+        self.__cluster_num = cluster_num   # 聚类数
+        self.__init_cluster_center = init_cluster_center
+        if init_cluster_center is not None:
+            self.__init_flag = False
+        else:
+            self.__init_flag = True
 
     def build(self, feature, label):
         """
@@ -27,6 +33,11 @@ class KMean(Model):
         """
         if isdata(feature) is not np.ndarray:
             feature = np.array(feature)   # 转换为矩阵
+        self.__feature_shape = feature.shape
+        # 初始化模型，簇中心初始化
+        self.init_cluster_center()
+        # 构建模型逻辑
+        self.model(feature, label, self.__cluster_num)
 
     def model(self, sample, label, cluster_amount=3):
         """
@@ -37,3 +48,21 @@ class KMean(Model):
         :return:
         """
         pass
+
+    def init_cluster_center(self):
+        """
+        初始化簇中心
+        :return:
+        """
+        if self.__init_flag:
+            self.__cluster_center = [np.random.random(self.__feature_shape) for i in range(self.__cluster_num)]
+        else:
+            pass
+
+    def set_seed(self, num):
+        """
+        设置初始化种子
+        :param num:
+        :return:
+        """
+        np.random.seed(num)
