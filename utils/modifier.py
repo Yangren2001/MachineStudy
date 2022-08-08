1fd8fb963af3ad9d8b48cdce273c42301af6ca65
Yangren2001 <2431206446@qq.com>
2022-08-08 21:28:34 +0800

    first test
    Signed-off-by: Yangren2001 <2431206446@qq.com>

--- utils/modifier.py
+++ utils/modifier.py
@@ -10,39 +10,83 @@ import tqdm
 import sys
 import time
 
-def changename(f):
-    """更改函数名"""
-    cname = f.__name__
-    c_doc = f.__doc__
-    def name(*arg, **kwargs):
-        res = None
-        if kwargs or arg:
-            res = f(*arg, **kwargs)
-        else:
-            res = f()
-        return res
-    name.__name__ = cname
-    name.__doc__ = c_doc
-    return name
-
-
 class Logging:
 
-    def __init__(self):
+    def __init__(self, fun):
         self.__logging = logging.getLogger()  # init
         self.__logging.setLevel(logging.INFO)
         self.__sh = logging.StreamHandler(stream=sys.stdout)
+        self.res = fun
 
-    def __call__(self, fun):
-        @changename
-        def wrap(*arg, **kwargs):
+    def __call__(self,*arg, **kwargs):
+        if kwargs or arg:
+            return self.res(*arg, **kwargs)
+        else:
+            return self.res()
+
+class Modifier:
+
+    def __new__(cls, *args, **kwargs):
+        return super(Modifier, cls).__new__(cls, *args, **kwargs)
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def changename(fun):
+        """更改函数名"""
+        cname = fun.__name__
+        c_doc = fun.__doc__
+        def change(f):
+            def name(*arg, **kwargs):
+                res = None
+                if kwargs or arg:
+                    res = f(*arg, **kwargs)
+                else:
+                    res = f()
+                return res
+
+            name.__name__ = cname
+            name.__doc__ = c_doc
+            return name
+        return change
+
+    @staticmethod
+    def changeFunName(f):
+        """更改函数名"""
+
+        cname = f.__name__
+        c_doc = f.__doc__
+        def name(*arg, **kwargs):
             res = None
             if kwargs or arg:
-                res = fun(*arg, **kwargs)
+                res = f(*arg, **kwargs)
             else:
-                res = fun()
+                res = f()
             return res
-        return wrap
+
+        name.__name__ = cname
+        name.__doc__ = c_doc
+        return name
+
+    @classmethod
+    def abc(cls, a):
+        print(cls, a)
+        def r(res):
+            print(res)
+            @Modifier.changename(res)
+            def wrap(*arg, **kwargs):
+                if kwargs or arg:
+                    return res(*arg, **kwargs)
+                else:
+                    return res()
+            return wrap
+        return r
+
 
 if __name__ == "__main__":
-    pass
+    @Modifier.abc(Modifier)
+    def b():
+        print(1)
+    b()
+    print(b.__name__)
